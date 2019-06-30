9b2f1842fd408ea2ead01e7194ab6155ac06575b
tomotetra <cure.tomoki@gmail.com>
2019-06-30 17:50:54 +0900

    fade image on scroll

--- public/js/index.js
+++ public/js/index.js
@@ -11,11 +11,31 @@ function fadein(className, offset = 0) {
 
 $(function(){
   // fade in on load
-  const fadein_class = '.fadeIn';
-  fadein(fadein_class);
+  const fadeinClass = '.fadeIn';
+  fadein(fadeinClass);
 
   // fadein on scroll
   $(window).scroll(function (){
-    fadein(fadein_class, 100);
+    fadein(fadeinClass, 100);
+  });
+
+
+  // fade out profile image on scroll
+  const targetElm = '#profile-img';
+  const triggerElm = '#content-right';
+  const triggerInitialTop = 400;
+  const maxFade = 10;
+
+  $(targetElm).css('transition', `0.1s`);
+  $(window).scroll(function (){
+    const elemPos = $(triggerElm).offset().top;
+    const scrollTop = $(window).scrollTop();
+    const currentTop = elemPos - scrollTop;
+    let fade = (triggerInitialTop - currentTop) * maxFade / triggerInitialTop;
+    if (fade < 2) { fade = 0; }
+    if ( maxFade > fade ) {
+      // console.log(fade)
+      $(targetElm).css('filter', `blur(${fade}px)`);
+    }
   });
 });
