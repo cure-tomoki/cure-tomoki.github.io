9c17b6b6bd536861ba8fa448850f935fd8ac35b4
tomotetra <cure.tomoki@gmail.com>
2019-11-13 22:32:48 +0900

    css variables

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,13 @@
 const postcssImport = require('postcss-import');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
+const postcssCustomProperties = require('postcss-custom-properties');
 
 module.exports = {
   plugins: [
     postcssImport(),
     autoprefixer(),
+    postcssCustomProperties(),
     cssnano({
       preset: [
         'default',
