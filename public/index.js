86b63f59364c4bfad55e9406498027f55ce76542
tomotetra <cure.tomoki@gmail.com>
2020-04-12 21:26:35 +0900

    style fix

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
 "use strict";
 
+// fade in sections
+var fadein_class = 'fadeIn';
+
 function fadein(className) {
   var offset = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
   var windowHeight = window.innerHeight;
@@ -13,9 +16,8 @@ function fadein(className) {
       element.classList.add('scrollIn');
     }
   }
-}
+} // fade in on load
 
-var fadein_class = 'fadeIn'; // fade in on load
 
 window.onload = function () {
   fadein(fadein_class);
@@ -25,3 +27,15 @@ window.onload = function () {
 window.onscroll = function () {
   fadein(fadein_class, 100);
 };
+
+document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    var href = this.getAttribute('href');
+    var target = document.querySelector(href);
+    if (!target) return;
+    target.scrollIntoView();
+    target.focus();
+    history.pushState(null, null, href);
+  });
+});
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,5 @@
+// fade in sections
+const fadein_class = 'fadeIn';
 function fadein(className, offset = 0) {
   const windowHeight = window.innerHeight;
   const elements = document.getElementsByClassName(className);
@@ -9,15 +11,27 @@ function fadein(className, offset = 0) {
     }
   }
 }
-
-const fadein_class = 'fadeIn';
-
 // fade in on load
 window.onload = function () {
   fadein(fadein_class);
 };
-
 // fadein on scroll
 window.onscroll = function () {
   fadein(fadein_class, 100);
 };
+
+
+
+document
+  .querySelectorAll('a[href^="#"]')
+  .forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      const href = this.getAttribute('href');
+      const target = document.querySelector(href);
+      if (!target) return;
+      target.scrollIntoView();
+      target.focus();
+      history.pushState(null, null, href);
+    });
+  });
