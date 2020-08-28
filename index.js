4159663a5963417cc823fc777acda0a43ac4801c
tomotetra <tomotetra@users.noreply.github.com>
2020-08-28 17:37:28 +0000

    Deploying to release from  @ 2cccbea51d8ac0d8717b3228b0fb07541605e64c 🚀

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// fade in sections
+var fadein_class = 'fadeIn';
+
+function fadein(className) {
+  var offset = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  var windowHeight = window.innerHeight;
+  var elements = document.getElementsByClassName(className);
+
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    var elemPos = element.getBoundingClientRect().top;
+
+    if (elemPos - windowHeight + offset < 0) {
+      element.classList.add('scrollIn');
+    }
+  }
+} // fade in on load
+
+
+window.onload = function () {
+  fadein(fadein_class);
+}; // fadein on scroll
+
+
+window.onscroll = function () {
+  fadein(fadein_class, 100);
+};
+
+document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    var href = this.getAttribute('href');
+    var target = document.querySelector(href);
+    if (!target) return;
+    target.scrollIntoView();
+    target.focus();
+    history.pushState(null, null, href);
+  });
+});
