2b65fb16104ccb93f6b673b1c4326c7a275e4608
tomotetra <tomotetra@users.noreply.github.com>
2020-09-06 06:55:10 +0000

    Deploying to release from  @ 6f3668bdff822308e33d656d15c90fbadb0b9423 🚀

--- _next/static/wjvPvvr9sCd3mQmcu9exe/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-798372356f9f280b62c5.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-f951d89e936730b65cd8.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
