20cb3e48db858ac472372df0fdbb9902e2e47a53
cure-tmk <cure.tomoki@gmail.com>
2020-12-30 11:12:42 +0000

    Deploying to dist from  @ 985801a0ab22f7fe5df70d6dd9a88ca2db663233 🚀

---
+++ _next/static/TJOSzuoweUorx7mDBroB2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1a41ddfcd0ef51fb7f0b.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-85785763783c1e33a9f3.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
