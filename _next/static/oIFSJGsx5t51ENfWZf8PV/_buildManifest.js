5be4967d5fcdd9e5b7142b2e2b98f9f9019d2e8a
cure-tmk <cure.tomoki@gmail.com>
2021-03-13 12:34:58 +0000

    Deploying to dist from  @ cfc48a69aa9b9b40aa901bd5595bc99a4cbd8daa 🚀

--- _next/static/oIFSJGsx5t51ENfWZf8PV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-6fe4eaab0dccf0568133.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-74eee45aa8e7e3ad03d0.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
