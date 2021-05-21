b1d1146a17185e63fe1d4058b8df5bd29e26efd0
cure-tmk <cure.tomoki@gmail.com>
2021-05-21 15:32:17 +0000

    Deploying to dist from  @ b92ad846c08949611937026025af0ee951664eea 🚀

---
+++ _next/static/5v1kuJvQXxda_eEqdUrxb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-59743b8df006a101628f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8edc1d1d55e870844335.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/85fDEse8Wi18OaUQ0jf7j/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex-442a0484263b4f68b16a.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-8edc1d1d55e870844335.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
