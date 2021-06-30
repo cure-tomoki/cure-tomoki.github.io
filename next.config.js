277515958dc7409bd720753fe394b57f0dbb9cb8
cure-tmk <cure.tomoki@gmail.com>
2021-06-30 19:39:14 +0900

    update dependencies

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { ESBuildMinifyPlugin } = require('esbuild-loader');
 const {
   PHASE_DEVELOPMENT_SERVER,
   PHASE_PRODUCTION_SERVER,
@@ -12,62 +11,24 @@ const ENVS = {
   PRODUCTION: 'production',
 };
 
-function useEsbuildMinify(config, options) {
-  const terserIndex = config.optimization.minimizer.findIndex(
-    (minimizer) => minimizer.constructor.name === 'TerserPlugin'
-  );
-  if (terserIndex > -1) {
-    config.optimization.minimizer.splice(
-      terserIndex,
-      1,
-      new ESBuildMinifyPlugin(options)
-    );
-  }
-}
-
-function useEsbuildLoader(config, options) {
-  const jsLoader = config.module.rules.find(
-    (rule) => rule.test && rule.test.test('.js')
-  );
-
-  if (jsLoader) {
-    jsLoader.use.loader = 'esbuild-loader';
-    jsLoader.use.options = options;
-  }
-}
-
-const useEnv = (p) => {
-  console.info(`NEXT PHASE: ${p}`);
-  switch (p) {
-    case PHASE_DEVELOPMENT_SERVER:
-      return ENVS.DEVELOPMENT;
-    case PHASE_PRODUCTION_SERVER:
-      return ENVS.PRODUCTION;
-    default:
-      console.warn('Invalid phase. Falling back to production.');
-      return ENVS.PRODUCTION;
-  }
-};
-
 module.exports = (phase, { _defaultConfig }) => {
+  const ENV = ((p) => {
+    console.info(`NEXT PHASE: ${p}`);
+    switch (p) {
+      case PHASE_DEVELOPMENT_SERVER:
+        return ENVS.DEVELOPMENT;
+      case PHASE_PRODUCTION_SERVER:
+        return ENVS.PRODUCTION;
+      default:
+        console.warn('Invalid phase. Falling back to production.');
+        return ENVS.PRODUCTION;
+    }
+  })(phase);
+
   return {
     env: {
-      ENV: useEnv(phase),
+      ENV,
       VERSION: package.version,
-    },
-    webpack: (config, { webpack }) => {
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          React: 'react',
-        })
-      );
-      useEsbuildMinify(config);
-      useEsbuildLoader(config, {
-        loader: 'tsx',
-        target: 'es2017',
-      });
-
-      return config;
     },
   };
 };
